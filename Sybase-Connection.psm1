5def62b3a055283e0686fd8437d1c1750cf1c764
saboroshi <38815641+saboroshi@users.noreply.github.com>
2022-11-22 11:05:18 +0100

    Add files via upload

---
+++ Sybase-Connection.psm1
@@ -0,0 +1,45 @@
+﻿<#	
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2021 v5.8.196
+	 Created on:   	2022. 10. 04. 15:05
+	 Created by:   	Cservenyi Szabolcs
+	 Organization: 	
+	 Filename:     	PostgreSQL-Connection.psm1
+	 Description:
+	 ReleaseNotes:
+	 Version:		1.0
+	-------------------------------------------------------------------------
+	 Module Name: 	PostgreSQL-Connection
+	===========================================================================
+#>
+
+function Sybase-Connect
+{
+	param (
+		$Username = "",
+		$Password = "",
+		$Server = "",
+        $Port = "",
+        $IP = "",
+		$DBName = "",
+		$Query = ""
+	)
+	$Connection = New-Object System.Data.Odbc.OdbcConnection
+	$Connection.ConnectionString = "driver={Adaptive Server Enterprise};dsn=$Server;db=$DBName;na=$IP,$Port;uid=$Username;pwd=$Password;"
+	$Cmd = New-Object System.Data.Odbc.OdbcCommand
+	$Cmd.CommandText = $Query
+	$Cmd.Connection = $Connection
+	$Adapter = New-Object System.Data.Odbc.OdbcDataAdapter         
+	$Adapter.SelectCommand = $Cmd
+	$DataSet = New-Object System.Data.DataSet
+	$Adapter.Fill($DataSet)
+	$DataSet.Tables[0]
+	$Connection.Close()
+}
+
+# Export-ModuleMember Sybase-Connection
+
+
+
+
+
---
+++ Sybase-TestConnection.psm1
@@ -0,0 +1,49 @@
+﻿<#	
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2021 v5.8.196
+	 Created on:   	2022. 10. 04. 15:05
+	 Created by:   	Cservenyi Szabolcs
+	 Organization: 	
+	 Filename:     	PostgreSQL-TestConnection.psm1
+	 Description:
+	 ReleaseNotes:
+	 Version:		1.0
+	-------------------------------------------------------------------------
+	 Module Name: 	PostgreSQL-TestConnection
+	===========================================================================
+#>
+
+function Sybase-TestConnection
+{
+	param (
+		$Username = "",
+		$Password = "",
+		$Server = "",
+        $IP = "",
+        $Port = "",
+		$DBName = ""
+	)
+	$ErrorActionPreference = 'Stop'
+	try
+	{
+		$Connection = New-Object System.Data.Odbc.OdbcConnection
+	    $Connection.ConnectionString = "driver={Adaptive Server Enterprise};dsn=$Server;db=$DBName;na=$IP,$Port;uid=$Username;pwd=$Password;"
+		$Connection.Open()
+		$true
+	}
+	catch
+	{
+		$false
+	}
+	finally
+	{
+		$Connection.Close()
+	}
+}
+
+# Export-ModuleMember Sybase-TestConnection
+
+
+
+
+
